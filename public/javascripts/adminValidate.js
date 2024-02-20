194ee896d04b63258fa48b897b37ad5feba26c26
ShaharAshe <shaharas30@gmail.com>
2024-02-20 23:37:41 +0200

    //

--- public/javascripts/adminValidate.js
+++ public/javascripts/adminValidate.js
@@ -15,23 +15,23 @@ const funcs = (function (){
 // ======================================================================
 
 const main = (function () {
-    return {
-        main_func: function () {
-            fetch(`/api/allData`)
-                .then((status)=> {
-                    if (status.status >= 200 && status.status < 300) {
-                        return Promise.resolve(status)
-                    } else {
-                        return Promise.reject(new Error(status.statusText))
-                    }
-                })
-                .then((response) => response.json())
-                .then((json) => {
-                    console.log(json)
-                    utilities.posts_place.innerHTML = ``
-                    json.forEach(post => {
-                        utilities.posts_place.innerHTML +=
-                            `<div class="col-12 col-sm-6 col-lg-4 text-center">
+    const build_page = ()=>{
+        console.log('Build page function called');
+        fetch(`/api/allData`)
+            .then((status) => {
+                if (status.status >= 200 && status.status < 300) {
+                    return Promise.resolve(status)
+                } else {
+                    return Promise.reject(new Error(status.statusText))
+                }
+            })
+            .then((response) => response.json())
+            .then((json) => {
+                console.log(json)
+                utilities.posts_place.innerHTML = ``
+                json.forEach(post => {
+                    utilities.posts_place.innerHTML +=
+                        `<div class="col-12 col-sm-6 col-lg-4 text-center">
                             <div class="card">
                                 <img src="/images/post_icon.png" class="card-img-top img-fluid d-none d-sm-block" alt="mars image">
                                     <div class="card-body">
@@ -41,19 +41,63 @@ const main = (function () {
                                         <p class="card-text">email: ${post["email"]}</p>
                                         <p class="card-text">phone number: ${post["phone"]}</p>
                                         <p class="card-text">price: ${post["price"]}</p>
-                                        <a href="#" class="btn btn-primary">approve</a>
-                                        <a href="#" class="btn btn-primary">delete</a>
+                                        <form class="row g-3" method="post" action="/">
+                                        <button type="button" class="btn btn-primary approve_data" value="${post["id"]}">approve</button>
+                                        <button type="button" class="btn btn-primary del_data" value="${post["id"]}">delete</button>
                                     </div>
                                 </div>
                             </div>`
-                    })
                 })
-                .catch((error) => {
-                    console.log(error);
-                });
-
-
-
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+    return {
+        main_func: function () {
+            build_page();
+            utilities.posts_place.addEventListener("click", function (event) {
+                if (event.target.classList.contains("approve_data")) {
+                    fetch(`/api/allData`, {
+                        method: 'PUT',
+                        headers: {
+                            'Content-Type': 'application/json', // Set the content type if sending JSON data
+                        },
+                        body: JSON.stringify({approve: "yes", postId: event.target.value}) // Convert data to JSON format if needed
+                    }).then(response => {
+                        if (!response.ok) {
+                            throw new Error(`HTTP error! Status: ${response.status}`);
+                        }
+                        return response.json(); // You can change this based on the response format
+                    }).then(data => {
+                        console.log('Success:', data);
+                    // Handle the response data as needed
+                    })
+                        .catch(error => {
+                            console.error('Error:', error);
+                            // Handle errors
+                        });
+                }else if (event.target.classList.contains("del_data")) {
+                    fetch(`/api/allData`, {
+                        method: 'DELETE',
+                        headers: {
+                            'Content-Type': 'application/json', // Set the content type if sending JSON data
+                        },
+                        body: JSON.stringify({postId: event.target.value}) // Convert data to JSON format if needed
+                    }).then(response => {
+                        if (!response.ok) {
+                            throw new Error(`HTTP error! Status: ${response.status}`);
+                        }
+                        return response.json();
+                    }).then(data => {
+                        console.log('Success:', data);
+                    }).catch(error => {
+                        console.error('Error:', error);
+                        // Handle errors
+                    });
+                    build_page();
+                }
+            })
         },
     }
 })()
--- routes/api.js
+++ routes/api.js
@@ -23,4 +23,42 @@ router.get('/allData', (req, res) => {
     })
 });
 
+router.put('/allData', (req, res) => {
+    db.Post.update({"approve": req.body.approve}, {
+        where: {
+            id: req.body.postId,
+        }
+    })
+        .catch((err) => {
+        // extensive error handling can be done here - you don't always need such a detailed error handling
+        if (err instanceof Sequelize.ValidationError) {
+            res.render('unsuccessfulPost', {title: 'Unsuccessful post', message: `Invalid input: ${err}`});
+        } else if (err instanceof Sequelize.DatabaseError) {
+            res.render('unsuccessfulPost', {title: 'Unsuccessful post', message: `Database error: ${err}`});
+        } else {
+            res.render('unsuccessfulPost', {title: 'Unsuccessful post', message: `Unexpected error: ${err} `});
+        }
+    })
+});
+
+
+router.delete('/allData', (req, res) => {
+    db.Post.destroy({
+        where:{
+            id: req.body.postId,
+        }
+    })
+        .catch((err) => {
+            // extensive error handling can be done here - you don't always need such a detailed error handling
+            if (err instanceof Sequelize.ValidationError) {
+                res.render('unsuccessfulPost', {title: 'Unsuccessful post', message: `Invalid input: ${err}`});
+            } else if (err instanceof Sequelize.DatabaseError) {
+                res.render('unsuccessfulPost', {title: 'Unsuccessful post', message: `Database error: ${err}`});
+            } else {
+                res.render('unsuccessfulPost', {title: 'Unsuccessful post', message: `Unexpected error: ${err} `});
+            }
+        })
+});
+
+
 module.exports = router;
